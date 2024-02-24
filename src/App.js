bf713d36a08cdfa62d34fccf110b19e343bdc706
zorac <kavithaajaymenon@gmail.com>
2024-02-24 20:10:28 +0000

    added about and projects pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Homepage from './Homepage-component/homepage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Projects from './project-component/project-component';
+import About from './about-component/about-component';
+import Home from './homepage-component/homepage-component';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about-me">About Me</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about-me" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/homepage-component/homepage-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is me and my info</p>
+    </div>
+  );
+}
+
+export default Home;
