5134880db09e1c49b0beb47b4d2437a0788516d2
zorac <kavithaajaymenon@gmail.com>
2024-02-26 16:16:13 +0000

    made nav changes and published dashboard

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return <nav className="nav">
-        <a href="/" className="site-title">Site Name</a>
+        <a href="/" className="site-title">Adithya's Analytics Portfolio </a>
         <ul>
             <li className="active"> 
                 <a href="/projects">Projects</a>
--- src/project-component/project-component.jsx
+++ src/project-component/project-component.jsx
@@ -8,12 +8,17 @@ function Projects() {
 
       <h3>Power BI Dashboard - Supermaret Sales Data</h3>
       <Iframe url="https://app.powerbi.com/view?r=eyJrIjoiMzEyNDE3ZTUtMzM5Yi00Y2Y4LWE4NzUtM2I5MGIzNWEwMGFlIiwidCI6IjEzZTNiMTg2LWM0NDYtNGFhYi05YzZkLTlhYjliYjc2ODE2YyIsImMiOjh9"
-        width="640px"
-        height="320px"
-        id="powerBI"
-        className=""
-        display="block"
-        position="relative"/>
+        top = "0" 
+        left = "0" 
+        bottom = "0" 
+        right = "0" 
+        width = "100%"
+        height = "100%"
+        border = "none" 
+        margin = "0"
+        padding = "0"
+        z-index = "999999"
+        position = "absolute"/>
     </div>
   );
 }
