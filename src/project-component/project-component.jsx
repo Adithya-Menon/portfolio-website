28ced5ac28e506989d8532961fd344fde715631e
zorac <kavithaajaymenon@gmail.com>
2024-02-26 16:05:17 +0000

    added dashboard

--- src/project-component/project-component.jsx
+++ src/project-component/project-component.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Iframe from 'react-iframe'
 
 function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>This is the Projects page where I'll showcase my work.</p>
+
+      <h3>Power BI Dashboard - Supermaret Sales Data</h3>
+      <Iframe url="https://app.powerbi.com/view?r=eyJrIjoiMzEyNDE3ZTUtMzM5Yi00Y2Y4LWE4NzUtM2I5MGIzNWEwMGFlIiwidCI6IjEzZTNiMTg2LWM0NDYtNGFhYi05YzZkLTlhYjliYjc2ODE2YyIsImMiOjh9"
+        width="640px"
+        height="320px"
+        id="powerBI"
+        className=""
+        display="block"
+        position="relative"/>
     </div>
   );
 }
