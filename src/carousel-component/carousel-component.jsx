8a5c83804e309905a990019319278752dc0b08a5
zorac <kavithaajaymenon@gmail.com>
2024-03-04 15:25:44 +0000

    added tabs on projects page

--- src/carousel-component/carousel-component.jsx
+++ src/carousel-component/carousel-component.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
 import "./carousel.css";
 import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function Carousel({data}) {
     const [slide, setSlide] = useState(0);
+    const navigate = useNavigate();
 
     const nextSlide  = () => {
         setSlide(slide === data.slides.length - 1 ? 0: slide + 1);
@@ -13,11 +15,15 @@ function Carousel({data}) {
     const prevSlide = () => {
         setSlide(slide === 0 ? data.slides.length - 1 : slide - 1);
     };
+
+    const projectClick = () => {
+        navigate('/projects'); 
+      };
     return (
         <div className="carousel">
             <BsArrowLeftCircleFill className="arrow arrow-left" onClick= {prevSlide} />
             {data.slides.map((item, idx) => {
-                return <img src = {item.src} alt = {item.alt} key = {idx} className= {slide === idx ? "slide": "slide slide-hidden"}/>
+                return <img src = {item.src} alt = {item.alt} key = {idx} className= {slide === idx ? "slide": "slide slide-hidden"} onClick={projectClick}/>
             })}
             <BsArrowRightCircleFill className="arrow arrow-right" onClick= {nextSlide} />
             <span className="indicators ">
--- src/project-component/project-component.jsx
+++ src/project-component/project-component.jsx
@@ -1,26 +1,69 @@
 import React from 'react';
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
+import Paper from "@mui/material/Paper";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import "./projectStyles.css"
 
 function Projects() {
+  const [value, setValue] = React.useState(0); // Start with the first tab selected
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <div>{children}</div>
+        )}
+      </div>
+    );
+  }
+  
+
   return (
     <div>
       <h1>Projects</h1>
-
-      <h3>Power BI Dashboard - Supermaret Sales Data</h3>
-      <Iframe url="https://app.powerbi.com/view?r=eyJrIjoiMzEyNDE3ZTUtMzM5Yi00Y2Y4LWE4NzUtM2I5MGIzNWEwMGFlIiwidCI6IjEzZTNiMTg2LWM0NDYtNGFhYi05YzZkLTlhYjliYjc2ODE2YyIsImMiOjh9"
-        top = "0" 
-        left = "0" 
-        bottom = "0" 
-        right = "0" 
-        width = "100%"
-        height = "100%"
-        border = "none" 
-        margin = "0"
-        padding = "0"
-        z-index = "999999"
-        position = "absolute"/>
+      <Paper square>
+        <Tabs
+          value={value}
+          textColor="primary"
+          indicatorColor="primary"
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <Tab label="Power BI dashboard" />
+          <Tab label="One-Page Infographic" />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <h3>Power BI Dashboard - Supermarket Sales Data</h3>
+          <Iframe url="https://app.powerbi.com/view?r=eyJrIjoiMzEyNDE3ZTUtMzM5Yi00Y2Y4LWE4NzUtM2I5MGIzNWEwMGFlIiwidCI6IjEzZTNiMTg2LWM0NDYtNGFhYi05YzZkLTlhYjliYjc2ODE2YyIsImMiOjh9"
+            top = "0" 
+            left = "0" 
+            bottom = "0" 
+            right = "0" 
+            width = "100%"
+            height = "100%"
+            border = "none" 
+            margin = "0"
+            padding = "0"
+            z-index = "999999"
+            position = "absolute"/>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {/* Content for your One-Page Infographic tab goes here */}
+        </TabPanel>
+      </Paper>
     </div>
   );
 }
 
 export default Projects;
+
